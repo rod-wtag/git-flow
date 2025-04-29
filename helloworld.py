99c17fb5e3cfce38878321704e4357424a70986d
rokydas-wd <roky.das@welldev.io>
2025-04-29 14:15:40 +0600

    hello

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,4 @@
 print("Hello World!")
 # This is a simple Python script that prints "Hello World!" to the console.
 # It serves as a basic example of how to write and run a Python program.
+# To run this script, save it as helloworld.py and execute it using the command:
